fe5ab834adfeb6f0deaedc0dc98363b69fcb4bac
kupapa <nslnet.k@gmail.com>
2011-11-25 13:52:47 +0900

    isNullEmpty isNullStr add

--- nslframework/src/jp/co/nsl/Utility/Utility.java
+++ nslframework/src/jp/co/nsl/Utility/Utility.java
@@ -19,5 +19,34 @@ public class Utility {
    	DecimalFormat df = new DecimalFormat(sb.toString());
    	return df.format(Integer.parseInt(num));
 	}
-
+	
+	public static boolean isNullEmpty(String value){
+	
+		if(value == null || value.trim().equals("")){
+			return true;
+		}else{
+			return false;
+		}	
+	}
+	public static String isNullStr(String value){
+		
+		if(value == null){
+			return "";
+		}else{
+			return value;
+		}
+	}
+	public static String inputConvZero(String value){
+		String result = "";
+		if (value==null){
+			result = "0";
+		}else{
+			if (value.trim().length()==0){
+				result = "0";
+			}else{
+				result = value.trim();
+			}
+		}
+		return result;
+	}	
 }
