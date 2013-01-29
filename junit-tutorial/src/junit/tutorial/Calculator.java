9b602ee75cd9e14a70736f39c0af7d2de3ad86f9
kupapa <nslnet.k@gmail.com>
2013-01-29 22:14:16 +0900

    JUnit実践入門
    第一章22ページまで

--- junit-tutorial/src/junit/tutorial/Calculator.java
+++ junit-tutorial/src/junit/tutorial/Calculator.java
@@ -4,7 +4,8 @@ public class Calculator {
 	public int multiply(int x,int y){
 		return x * y;
 	}
-	public int divide(int x, int y){
-		return x / y;
+	public float divide(int x, int y){
+		if(y==0) throw new IllegalArgumentException("devide by zero.");
+		return (float)x / (float)y;
 	}
 }
--- junit-tutorial/test/junit/tutorial/CalculatorTest.java
+++ junit-tutorial/test/junit/tutorial/CalculatorTest.java
@@ -8,10 +8,31 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	@Test
-	public void multiplyで乗算結果が取得できる(){
+	public void multiplyで３と４の乗算結果が取得できる(){
 		Calculator calc = new Calculator();
 		int expected = 12;
 		int actual = calc.multiply(3,4);
 		assertThat(actual, is(expected));
 	}
+	 @Test
+	 public void multiplyで５と７の乗算結果が取得出来る(){
+		 Calculator calc = new Calculator();
+		 int expected = 35;
+		 int actual = calc.multiply(5, 7);
+		 assertThat(actual, is(expected));
+	 }
+	
+	 @Test
+	 public void divideで３と２の除算結果が取得出来る(){
+		 Calculator calc = new Calculator();
+		 float expected = 1.5f;
+		 float actual = calc.divide(3, 2);
+		 assertThat(actual, is(expected));
+	 }
+	 @Test(expected = IllegalArgumentException.class)
+	 public void divideで５と０のときIllegalArgumentExceptionを送出する(){
+		 Calculator calc = new Calculator();
+		 calc.divide(5, 0);
+	 }
+	 
 }
